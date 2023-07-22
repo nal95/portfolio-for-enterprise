e323278b272a374b884fb5935e3574c77cb403fc
nal95 <alex.nguepi@yahoo.fr>
2023-07-22 16:14:09 +0200

    Init project

---
+++ src/main/java/com/nal95/portfolioforenterprise/PortfolioForEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.nal95.portfolioforenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioForEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioForEnterpriseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nal95/portfolioforenterprise/PortfolioForEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nal95.portfolioforenterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioForEnterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
